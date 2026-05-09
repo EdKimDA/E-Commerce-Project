3572130055d7cede18b06d30b9f54cf84e62c28b
EdKimDA <twux9522@gmail.com>
2026-05-09 18:53:56 +0500

    Added OrderItem entity

---
+++ src/main/java/com/example/kimeduardfinalproject/entities/OrderItem.java
@@ -0,0 +1,46 @@
+package com.example.kimeduardfinalproject.entities;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @NotBlank(message = "Product name must not be empty")
+    @Column(nullable = false, length = 150)
+    private String productName;
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal priceAtPurchase;
+
+    @Min(value = 1, message = "Quantity must be at least 1")
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "Subtotal must be greater than 0")
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal subtotal;
+}
