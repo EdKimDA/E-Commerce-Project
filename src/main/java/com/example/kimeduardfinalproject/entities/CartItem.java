27dd9a9e5681f3d4b2e53a2e77ff577f19c28e79
EdKimDA <twux9522@gmail.com>
2026-05-09 18:30:37 +0500

    Added CartItem Entity

---
+++ src/main/java/com/example/kimeduardfinalproject/entities/CartItem.java
@@ -0,0 +1,39 @@
+package com.example.kimeduardfinalproject.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cart_items", uniqueConstraints = @UniqueConstraint(columnNames = {"cart_id", "product_id"}))
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Min(value = 1, message = "Quantity must be at least 1")
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.quantity == null) {
+            this.quantity = 1;
+        }
+    }
+}
