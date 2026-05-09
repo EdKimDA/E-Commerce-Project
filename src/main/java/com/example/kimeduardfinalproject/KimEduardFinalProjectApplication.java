968cf1379378e11a9e1fbefed1cef8277f039240
EdKimDA <twux9522@gmail.com>
2026-05-09 17:57:02 +0500

    first commit

---
+++ src/main/java/com/example/kimeduardfinalproject/KimEduardFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kimeduardfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimEduardFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimEduardFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kimeduardfinalproject/KimEduardFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kimeduardfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimEduardFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
